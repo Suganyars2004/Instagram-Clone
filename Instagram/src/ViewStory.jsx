ed8bfb33362d7ebea174a8574e4253cec8981fba
unknown <suganyars2004@gmail.com>
2025-09-28 22:55:11 +0530

    sep 28

--- Instagram/src/ViewStory.jsx
+++ Instagram/src/ViewStory.jsx
@@ -1,13 +1,26 @@
 import React, { use, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams,Link, useNavigate } from 'react-router-dom'
 function ViewStory() {
-    const {id} =useParams();
+    const {id,total} =useParams();
     const [story,setStory]=useState(null);
+    const navigate=useNavigate();
     useEffect(()=>{
-        fetch("http://localhost:3000/story")
-    })
+        fetch(`http://localhost:3000/story/${id}`)
+        .then((data=>data.json()))
+        .then(data=>setStory(data))
+        .catch(err=>console.log(err))
+    },[id])
+    if(id>total || id<=0){
+      navigate('/');
+    }
   return (
-    <div></div>
+    <div>
+      {story ? <div className='d-flex justify-content-center align-items-center'>
+        <Link to={`http://localhost:5173/story/${Number(id)-1}/${total}`}><i className='bi bi-arrow-left-circle-fill'></i></Link>
+        <img className='vh-100 ' src={story.imageUrl} alt="" />
+        <Link to={`http://localhost:5173/story/${Number(id)+1}/${total}`}><i className='bi bi-arrow-right-circle-fill'></i></Link>
+      </div> :<div>Loading</div>}
+    </div>
   )
 }
 
